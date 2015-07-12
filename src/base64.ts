f6c10926daa45124afbfe2d9413838c0b751d894
Jamie C <jcoup@me.com>
2015-07-12 16:49:29 +0100

    removed tsd dependecy

--- src/base64.ts
+++ src/base64.ts
@@ -1,5 +1,5 @@
 ///<reference path="../typings/base64-js/b64.d.ts" />
-import {toByteArray, fromByteArray} from 'base64';
+import 'base64';
 import {UTF8} from './utf8';
 
 
@@ -9,12 +9,12 @@ export class Base64 {
    * Pass the Base64 value
    */
   static getBytes(value: string): Uint8Array {
-    return toByteArray(value);
+    return base64js.toByteArray(value);
   }
 
 
   static getString(value: Uint8Array): string {
-    return fromByteArray(value);
+    return base64js.fromByteArray(value);
   }
 
 }
